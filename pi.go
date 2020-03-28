e5b96fc9089217e7d8f6c7b07cd3544031638894
NguyenNK <nguyennk92@gmail.com>
2020-03-28 17:48:56 +0700

    Initial commits

---
+++ pi.go
@@ -0,0 +1,22 @@
+package common
+
+import (
+	"math/rand"
+)
+
+func Pi(num int) float64 {
+	rand.Seed(100)
+	count1 := 0
+	count2 := 0
+	for i := 0; i < num; i++ {
+		x := rand.Float64()
+		y := rand.Float64()
+		d := x*x + y*y
+		if d <= 1 {
+			count1++
+		}
+		count2++
+	}
+
+	return 4 * (float64(count1) / float64(count2))
+}
